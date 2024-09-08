2af6b7f491688a682c99e7437cea56a8d5cf1560
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-09-08 18:48:01 +0530

    Updated the Backend URL

--- user-management/src/app/shared/services/user.service.ts
+++ user-management/src/app/shared/services/user.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
 
-  private apiUrl = 'https://user-management-backend-sage.vercel.app/users';
+  private apiUrl = 'https://user-management-backend-xml9.onrender.com/users';
 
   constructor(private http: HttpClient) { }
 
